ef50f65a205a09cda82b5cc7ae29e6a3d354a445
IMRANDIL <aliimranadil2@gmail.com>
2024-04-06 18:50:36 +0530

    added pdf package and implementation

--- generate/generateDoc.go
+++ generate/generateDoc.go
@@ -1,1 +1,41 @@
+// marketdata/generate/pdf.go
+
 package generate
+
+import (
+	"fmt"
+
+	"github.com/imrandil/the_real_world/models"
+	"github.com/jung-kurt/gofpdf"
+)
+
+// GeneratePDF generates a PDF file containing the market data records
+func GeneratePDF(items []*models.MarketData) error {
+	// Initialize PDF
+	pdf := gofpdf.New("P", "mm", "A4", "")
+
+	// Add a page
+	pdf.AddPage()
+
+	// Set font
+	pdf.SetFont("Arial", "", 12)
+
+	// Add content to the PDF
+	pdf.Cell(40, 10, "Market Data Records")
+	pdf.Ln(-1) // Move to the next line
+
+	// Add retrieved data to the PDF
+	for _, item := range items {
+		row := fmt.Sprintf("State: %s, District: %s, Market: %s, Commodity: %s, Variety: %s, Arrival Date: %s, Min Price: %d, Max Price: %d, Modal Price: %d",
+			item.State, item.District, item.Market, item.Commodity, item.Variety, item.ArrivalDate, item.MinPrice, item.MaxPrice, item.ModalPrice)
+		pdf.CellFormat(0, 10, row, "", 1, "L", false, 0, "")
+	}
+
+	// Save the PDF file
+	err := pdf.OutputFileAndClose("market_data.pdf")
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/imrandil/the_real_world
 
 go 1.22.2
 
-require github.com/lib/pq v1.10.9
+require (
+	github.com/jung-kurt/gofpdf v1.16.2
+	github.com/lib/pq v1.10.9
+)
